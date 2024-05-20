a6c275beea19c5b311f684e188eb03fbacac1066
Vladimir Seldemirov <ligowsky.dev@gmail.com>
2024-05-21 00:18:26 +0400

    Fix summary

--- src/BitzArt.Blazor.MVVM/Models/StateManager.cs
+++ src/BitzArt.Blazor.MVVM/Models/StateManager.cs
@@ -5,7 +5,7 @@ namespace BitzArt.Blazor.MVVM;
 public interface IStateManager
 {
     /// <summary>
-    /// Encodes <see cref="ViewModel"/>'s and it's nested <see cref="ViewModel"/>' states into a base64 string.
+    /// Encodes <see cref="ViewModel"/>'s and it's nested <see cref="ViewModel"/>s' states into a base64 string.
     /// </summary>
     public string EncodeState(ViewModel viewModel);
 }
