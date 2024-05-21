cdb64c2c4c6974027ace68e8500dc45ccd034125
Vladimir Seldemirov <ligowsky.dev@gmail.com>
2024-05-21 12:09:50 +0400

    Extract IStateManager

---
+++ src/BitzArt.Blazor.MVVM/Interfaces/IStateManager.cs
@@ -0,0 +1,9 @@
+﻿namespace BitzArt.Blazor.MVVM;
+
+public interface IStateManager
+{
+    /// <summary>
+    /// Encodes <see cref="ViewModel"/>'s and it's nested <see cref="ViewModel"/>s' states into a base64 string.
+    /// </summary>
+    public string? EncodeState(ViewModel viewModel);
+}
